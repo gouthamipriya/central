da2c5708afcfb7742a3d5facca32e24de703e6c0
gouthami <mukkapallygouthamipriya@gmail.com>
2018-10-28 20:09:52 -0500

    Update src/Central/Addition.java

--- src/Central/Addition.java
+++ src/Central/Addition.java
@@ -3,6 +3,11 @@ package Central;
 public class Addition {
 
 	public static void main(String[] args) {
+		int a=10; int b=20;
+		int c;
+		c=a+b;
+		System.out.println("c =" +c);
+		
 		// TODO Auto-generated method stub
 
 	}
