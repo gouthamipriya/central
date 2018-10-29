6f4ddc9b7e5f5d885d1fc9818c56b9d01af8dfae
gouthami <mukkapallygouthamipriya@gmail.com>
2018-10-28 20:16:46 -0500

    added subst

--- src/Central/Addition.java
+++ src/Central/Addition.java
@@ -6,7 +6,9 @@ public class Addition {
 		int a=10; int b=20;
 		int c;
 		c=a+b;
+		c=a-b;
 		System.out.println("c =" +c);
+		System.out.println("c="+c);
 		
 		// TODO Auto-generated method stub
 
