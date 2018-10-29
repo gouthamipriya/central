8170acf8c894337b495ce3147d583760d5b7b398
gouthami <mukkapallygouthamipriya@gmail.com>
2018-10-28 20:46:44 -0500

    added div

--- src/Central/Addition.java
+++ src/Central/Addition.java
@@ -7,7 +7,9 @@ public class Addition {
 		int c;
 		c=a+b;
 		c=a-b;
+		c=a%b;
 		System.out.println("c =" +c);
+		System.out.println("c="+c);
 		System.out.println("c="+c);
 		
 		// TODO Auto-generated method stub
